8c8cd7532673d3cc2d06742017cb5fab605131c1
Corey Bruyere <coreybruyere@ymail.com>
2019-03-11 19:25:42 -0700

    Fix broken gatsby config font package and import CSS font

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,19 +27,19 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Martel`,
-            subsets: [`serif`],
-          },
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       {
+    //         family: `Martel`,
+    //         subsets: [`serif`],
+    //       },
+    //     ],
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    "gatsby-plugin-offline",
+    // "gatsby-plugin-offline",
   ],
 }
